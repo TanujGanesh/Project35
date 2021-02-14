e23ef3283d0668d0bf8ebb4aa10601d271b5adab
TanujGanesh <74528139+TanujGanesh@users.noreply.github.com>
2021-02-14 20:05:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var balloon,balloonAni;
+var database,position,bp;
+var backgroundImg;
+var y1=300;
+
+function preload(){
+    backgroundImg = loadImage('HotAirBallon01.jpg');
+    balloonAni = loadAnimation('Hot02.png','Hot03.png','Hot04.png');
+}
+
+function setup(){
+    database=firebase.database();
+    createCanvas(700,600);
+    balloon = createSprite(250,250,50,50);
+    balloon.scale=0.4;
+    balloon.addAnimation('t',balloonAni);
+    bp = database.ref('balloon/position')
+    bp.on("value",readPosition);
+}
+
+function draw(){
+    background(backgroundImg,700,600);
+    fill(0);
+    text("**use arrow keys to move the balloon**",20,30);
+    
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)&& y1>=50){
+        balloon.scale=balloon.scale -0.01;
+        changePosition(0,-10);
+        y1-= 10;
+    }
+    else if(keyDown(DOWN_ARROW)&& y1<=450){
+        balloon.scale=balloon.scale +0.01;
+        changePosition(0,+10);
+        y1+= 10;
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('balloon/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+      })
+    
+}
+
+function readPosition(data){
+    position = data.val();
+    balloon.x = position.x;
+    balloon.y = position.y;
+}
